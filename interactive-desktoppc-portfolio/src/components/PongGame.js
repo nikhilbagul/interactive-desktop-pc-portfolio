92b5d21e2cc6a674c649a124a2bb21b5a470ca67
nikhilbagul <nik2522@gmail.com>
2024-04-17 23:50:27 -0700

    Add iframe for Pong as a react container
    - Add flex row layout for desktop icons
    - Add windows taskbar

---
+++ interactive-desktoppc-portfolio/src/components/PongGame.js
@@ -0,0 +1,19 @@
+import React from "react";
+import './PongGame.css';
+
+const PongIframeComponent = () => {
+    return (
+        <div className="pong-iframe-container">
+            <iframe
+                src="https://pong-unity-clone.vercel.app/" // Change this to the URL you want to embed
+                width="1280" // Set the width to 1280
+                height="720" // Set the height to 720
+                frameBorder="0" // Removes the border around the iframe
+                allowFullScreen // Allows the iframe to go fullscreen
+            >
+            </iframe>
+        </div>
+    );
+};
+
+export default PongIframeComponent;
--- interactive-desktoppc-portfolio/src/components/pages/DesktopScreen.js
+++ interactive-desktoppc-portfolio/src/components/pages/DesktopScreen.js
@@ -1,15 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import './DesktopScreen.css'
+import PongIframeComponent from "../PongGame";
 
-function DesktopScreen() {
+function DesktopScreen () {
+
+    // change component state when user request to open any app from desktop screen
+    const [showPongGame, setDesktopScreenState] = useState(false);
+    const togglePongGameContainer = () =>
+    {
+        setDesktopScreenState(!showPongGame);
+    }
+
+
+    // Handle button click event
+    const handleClick = () => {
+        console.log('Button clicked!  Load Pong Game');
+        togglePongGameContainer();        
+    };
 
     return (
+        <div>
+            <div className="bg-image"></div>            
+            <div className="hero-container-desktopScreen">
+                <img className = "pong-icon" src="PongIcon.png" alt="Launch Pong" onClick={handleClick}/>
+                <img className = "youtube-icon" src="YoutubeIcon.png" alt="Launch Youtube" />
+                <img className = "pong-icon" src="PongIcon.png" alt="Button" />
 
-        <div className="hero-container-desktopScreen">            
-            <div className="bg-image"></div>
-            <img className = "pong-icon" src="PongIcon.jpg" alt="Button" />
+                { showPongGame && <PongIframeComponent/> }              
+            </div>            
+            <img className = "taskbar" src="WindowsTaskbar.jpg" />
+
         </div>
-
     );
 };
 
