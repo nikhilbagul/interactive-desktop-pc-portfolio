8c0d0adbc9c604d4539375dd2821fe0c3b5437da
nikhilbagul <nik2522@gmail.com>
2024-05-01 23:19:55 -0700

    Add Start menu os label and buttons

--- src/components/pages/StartMenu.js
+++ src/components/pages/StartMenu.js
@@ -5,8 +5,11 @@ function StartMenu () {
     return(
     <div>
         <div id="start-menu">
-            <div id="os-label">
+            <div id="os-label">                
                 <p id="os-label-text">NootNoot OS</p>
+                <img id="power-button" src='powerIcon.png' />
+                <img id="settings-button" src='settingsIcon.png' />
+                <img id="power-button" src='powerIcon.png' />
             </div>
         </div>                
     </div>
