9cf0e506b19f0bf89b3526822ad0d13f34a50a25
nikhilbagul <nik2522@gmail.com>
2026-01-26 09:22:46 -0800

    Add title property to Pong Iframe component

--- src/components/iframeComponents/PongGame.js
+++ src/components/iframeComponents/PongGame.js
@@ -5,6 +5,7 @@ const PongIframeComponent = () => {
     return (
         <div className="pong-iframe-container">
             <iframe id="pong-iframe"
+                title="pong-game"
                 src="https://pong-unity-clone.vercel.app/" // Change this to the URL you want to embed
                 width="1280" // Set the width to 1280
                 height="720" // Set the height to 720
